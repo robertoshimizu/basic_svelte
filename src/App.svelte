2f9d708c30a5cc7d66c25138322460bec872c716
Roberto Shimizu <rshimizubr@gmail.com>
2023-03-31 21:49:58 -0300

    style: :lipstick: readequate right html tags

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+  import NavBar from './lib/Navbar.svelte'
 
   const vite_href = 'https://vitejs.dev'
   const git_href = "https://github.com/sveltejs/kit#readme"
   const href='https://svelte.dev'
 </script>
 
+
+  
+<header>
+  <NavBar />
+  <a href={vite_href} target="_blank" rel="noreferrer"> 
+    <img src="/vite.svg" class="logo" alt="Vite Logo" />
+  </a>
+  <a {href} target="_blank" rel="noreferrer"> 
+    <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+  </a>
+    <h1>Vite + Svelte</h1>
+</header>
 <main>
-  <div>
-    <a href={vite_href} target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a {href} target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
+  <Counter />
   <p>
     Check out <a href={git_href} target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts"></script>
+
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+</nav>
+<style>
+  nav {
+    background: #333;
+    color: #fff;
+    padding: 1rem;
+  }
+  ul {
+    list-style: none;
+    display: flex;
+  }
+  li {
+    margin-right: 1rem;
+  }
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+</style>
